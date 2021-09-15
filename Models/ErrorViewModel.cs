6d927cb4f95908297389fda202b9032e8e62e31c
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-09-14 22:16:54 -0300

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace teste_core_IIS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
